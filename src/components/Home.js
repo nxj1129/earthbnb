ed2d4a4c47c838e0c4cd19611ce6a217b6057279
nxj1129 <niko.jugovac@gmail.com>
2021-03-30 13:13:50 +0200

    Added Banner and DatePicker, small UI updates

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "../css/Home.css";
+import Banner from "./Banner.js";
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home component</h1>
+      <Banner />
     </div>
   );
 }
